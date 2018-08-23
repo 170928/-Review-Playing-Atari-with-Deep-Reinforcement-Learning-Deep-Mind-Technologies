697de416e4dabc30ea72ade736d387162ca84e6e
Ashikandi <mjshin.cau@gmail.com>
2018-08-23 21:34:05 +0900

    DQN
    (1) Not completed

--- atari.py
+++ atari.py
@@ -14,9 +14,12 @@ from argparse import ArgumentParser
 from tensorflow.contrib.layers import xavier_initializer
 from  skimage.color import rgb2gray
 from skimage.transform import resize
+from collections import deque, namedtuple
+#from lib import plotting
 
 env = gym.make('BreakoutDeterministic-v4')
 IMGSIZE = 84
+VALID_ACTIONS = [0, 1, 2, 3]
 
 
 # Frame을 Convoludtion 2D를 위해 84x84 형태의 사각형으로.
@@ -49,7 +52,7 @@ def get_copy_var_ops(*, sess, dest_scope_name="target", src_scope_name="main"):
 
 class DQN:
 
-    def __init__(self, sess, name, summaries_dir=None):
+    def __init__(self, name, summaries_dir=None):
 
         self.summary_writer = None
         self.name = name
@@ -60,7 +63,7 @@ class DQN:
         self.forward()
 
         if summaries_dir:
-            summary_dir = os.path.join(summaries_dir, "summaries_{}".format(scope))
+            summary_dir = os.path.join(summaries_dir, "summaries_{}".format(name))
             if not os.path.exists(summary_dir):
                 os.makedirs(summary_dir)
             self.summary_writer = tf.summary.FileWriter(summary_dir)
@@ -74,14 +77,14 @@ class DQN:
             # The target value
             self.Y = tf.placeholder(tf.float32, [self.size_batch] )
             # Index of selected action
-            self.action = tf.placeholder(tf.float32, [self.size_batch] )
+            self.action = tf.placeholder(tf.uint8, [self.size_batch] )
             # Frame input 84x84x4
-            self.X = tf.placeholder(tf.uint8, [self.size_batch, IMGSIZE, IMGSIZE, self.num_frame] )
+            self.X = tf.placeholder(tf.uint8, [self.size_batch, IMGSIZE, IMGSIZE, 4] )
             self.frames = tf.to_float(self.X)/255.0
 
             self.f1 = tf.get_variable(name='conv_w1', shape=[8,8,4,16], initializer=xavier_initializer())
             self.f2 = tf.get_variable(name='conv_w2', shape=[4,4,16,32], initializer=xavier_initializer())
-            self.w1 = tf.get_variable(name='fc_w1',shape=[11*8*32 ,self.num_hidden], initializer=xavier_initializer())
+            self.w1 = tf.get_variable(name='fc_w1',shape=[2592 ,self.num_hidden], initializer=xavier_initializer())
             self.w2 = tf.get_variable(name='fc_w2', shape=[self.num_hidden ,self.num_action], initializer=xavier_initializer())
 
             h1 = tf.nn.conv2d(self.frames, self.f1, strides=[1,4,4,1], padding='VALID')
@@ -91,13 +94,13 @@ class DQN:
             # input의 형태가 [batch_size, ....]일때
             # input을 [batch_size, -1] 로 변환시켜 준다.
             h2 = tf.contrib.layers.flatten(h2)
-            fc1 = tf.matmul(h2, w1)
+            fc1 = tf.matmul(h2, self.w1)
             fc1 = tf.nn.relu(fc1)
             # out 변수는 DQN을 거친 결과로 [batch_size, num_action]
-            self.pred_qval= tf.matmul(fc1, w2)
+            self.pred_qval= tf.matmul(fc1, self.w2)
 
             # Get the predictions for the chosen actions only
-            mask = tf.one_hot(action, 4, on_value=1, off_value=0)
+            mask = tf.one_hot(self.action, 4, on_value=1, off_value=0)
             mask = tf.to_float(mask)
 
             pred_selected_qval = tf.multiply(self.pred_qval, mask)
@@ -109,11 +112,11 @@ class DQN:
 
             # Optimalizer
             self.optimizer = tf.train.RMSPropOptimizer(0.00025, 0.95,0.0,1e-6)
-            self.train_op = self.optimizer.minimize(self.loss)
+            self.train_op = self.optimizer.minimize(self.loss, global_step=tf.contrib.framework.get_global_step())
 
             # Tensorboard
             self.summaries = tf.summary.merge([
-                tf.summary.scalar("Loss", self.loss)
+                tf.summary.scalar("Loss", self.loss),
                 tf.summary.scalar("Average Q Var", tf.reduce_mean(self.pred_action))
             ])
 
@@ -122,8 +125,8 @@ class DQN:
 
     def update(self, sess, state, action, y):
         feed_dict = {self.X : state, self.action :action, self.Y : y}
-        summaries,  _, loss = sess.run(
-            [self.summaries, self.train_op, self.loss], feed_dict)
+        summaries, step, _, loss = sess.run(
+            [self.summaries, tf.contrib.framework.get_global_step() ,self.train_op, self.loss], feed_dict)
         if self.summary_writer:
             self.summary_writer.add_summary(summaries)
         return loss
@@ -138,6 +141,7 @@ def make_epsilon_greedy_policy( model , num_action ):
         best_action = np.argmax(q_values)
 
         temp[best_action] += (1.0 - epsilon)
+        # 확률 값의 array를 반환해주도록 하는 함수
         return temp
     return policy_fn
 
@@ -192,15 +196,12 @@ def deep_q_learning(sess,
 
     Transition = namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
 
-    # Replay memory
+    # Replay memory =======================================================
     replay_memory = []
+    # =====================================================================
 
-    # Keeps track of useful statistics
-    stats = plotting.EpisodeStats(
-        episode_lengths=np.zeros(num_episodes),
-        episode_rewards=np.zeros(num_episodes))
 
-    # Create directories for checkpoints and summaries
+    # Directory Check ====================================================
     checkpoint_dir = os.path.join(experiment_dir, "checkpoints")
     checkpoint_path = os.path.join(checkpoint_dir, "model")
     monitor_path = os.path.join(experiment_dir, "monitor")
@@ -209,40 +210,54 @@ def deep_q_learning(sess,
         os.makedirs(checkpoint_dir)
     if not os.path.exists(monitor_path):
         os.makedirs(monitor_path)
+    # =====================================================================
 
+    # Save Information Check ==============================================
     saver = tf.train.Saver()
-    # Load a previous checkpoint if we find one
     latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
     if latest_checkpoint:
         print("Loading model checkpoint {}...\n".format(latest_checkpoint))
         saver.restore(sess, latest_checkpoint)
+    # =====================================================================
 
-    total_t = sess.run(tf.contrib.framework.get_global_step())
+    # Epsilon Gradient ====================================================
+    # Epsilon Gradient 를 위해서 학습 과정에서의 Step Information을 받아와야한다.
+    # 이를 위해서 optimizer.minimize(global_step=tf.contrib.framework.get_global_step()) 와 같이 선언을 해두었다.
+    total_step = sess.run(tf.contrib.framework.get_global_step())
 
-    # The epsilon decay schedule
+    # Epsilon Decay 방법을 정의 =============================================
     epsilons = np.linspace(epsilon_start, epsilon_end, epsilon_decay_steps)
 
-    # The policy we're following
     policy = make_epsilon_greedy_policy(
-        q_estimator,
-        len(VALID_ACTIONS))
+        main_model,
+        4)
+    # =====================================================================
 
-    # Populate the replay memory with initial experience
-    print("Populating replay memory...")
+    # Replay memory =======================================================
+    # Frame 전처리================================
     state = env.reset()
     state = state_processor.process(sess, state)
     state = np.stack([state] * 4, axis=2)
+    # ===========================================
+
     for i in range(replay_memory_init_size):
-        action_probs = policy(sess, state, epsilons[min(total_t, epsilon_decay_steps-1)])
+
+        action_probs = policy(sess, state, epsilons[min(total_step, epsilon_decay_steps-1)])
         action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-        next_state, reward, done, _ = env.step(VALID_ACTIONS[action])
+
+        next_state, reward, done, _ = env.step(VALIDE_ACTIONS[action])
+
         next_state = state_processor.process(sess, next_state)
+
         next_state = np.append(state[:,:,1:], np.expand_dims(next_state, 2), axis=2)
+
         replay_memory.append(Transition(state, action, reward, next_state, done))
+
         if done:
             state = env.reset()
             state = state_processor.process(sess, state)
             state = np.stack([state] * 4, axis=2)
+
         else:
             state = next_state
 
@@ -253,36 +268,38 @@ def deep_q_learning(sess,
                   resume=True,
                   video_callable=lambda count: count % record_video_every ==0)
 
+
     for i_episode in range(num_episodes):
 
         # Save the current checkpoint
         saver.save(tf.get_default_session(), checkpoint_path)
 
-        # Reset the environment
+        # Frame 전처리================================
         state = env.reset()
         state = state_processor.process(sess, state)
         state = np.stack([state] * 4, axis=2)
         loss = None
+        # ===========================================
 
-        # One step in the environment
+        # t = 1 to T ================================
         for t in itertools.count():
 
             # Epsilon for this time step
-            epsilon = epsilons[min(total_t, epsilon_decay_steps-1)]
+            epsilon = epsilons[min(total_step, epsilon_decay_steps-1)]
 
             # Add epsilon to Tensorboard
             episode_summary = tf.Summary()
             episode_summary.value.add(simple_value=epsilon, tag="epsilon")
-            q_estimator.summary_writer.add_summary(episode_summary, total_t)
+            main_model.summary_writer.add_summary(episode_summary, total_step)
 
             # Maybe update the target estimator
-            if total_t % update_target_estimator_every == 0:
-                copy_model_parameters(sess, q_estimator, target_estimator)
+            if total_step % update_target_estimator_every == 0:
+                get_copy_var_ops(sess, 'target', 'main')
                 print("\nCopied model parameters to target network.")
 
             # Print out which step we're on, useful for debugging.
             print("\rStep {} ({}) @ Episode {}/{}, loss: {}".format(
-                    t, total_t, i_episode + 1, num_episodes, loss), end="")
+                    t, total_step, i_episode + 1, num_episodes, loss), end="")
             sys.stdout.flush()
 
             # Take a step
@@ -299,41 +316,35 @@ def deep_q_learning(sess,
             # Save transition to replay memory
             replay_memory.append(Transition(state, action, reward, next_state, done))
 
-            # Update statistics
-            stats.episode_rewards[i_episode] += reward
-            stats.episode_lengths[i_episode] = t
 
             # Sample a minibatch from the replay memory
             samples = random.sample(replay_memory, batch_size)
             states_batch, action_batch, reward_batch, next_states_batch, done_batch = map(np.array, zip(*samples))
 
             # Calculate q values and targets (Double DQN)
-            q_values_next = q_estimator.predict(sess, next_states_batch)
+            q_values_next = main_model.pred(sess, next_states_batch)
             best_actions = np.argmax(q_values_next, axis=1)
-            q_values_next_target = target_estimator.predict(sess, next_states_batch)
+
+            q_values_next_target = target_model.pred(sess, next_states_batch)
+
             targets_batch = reward_batch + np.invert(done_batch).astype(np.float32) * \
                 discount_factor * q_values_next_target[np.arange(batch_size), best_actions]
 
             # Perform gradient descent update
             states_batch = np.array(states_batch)
-            loss = q_estimator.update(sess, states_batch, action_batch, targets_batch)
+            loss = main_model.update(sess, states_batch, action_batch, targets_batch)
 
             if done:
                 break
 
             state = next_state
-            total_t += 1
+            total_step += 1
 
         # Add summaries to tensorboard
         episode_summary = tf.Summary()
-        episode_summary.value.add(simple_value=stats.episode_rewards[i_episode], node_name="episode_reward", tag="episode_reward")
-        episode_summary.value.add(simple_value=stats.episode_lengths[i_episode], node_name="episode_length", tag="episode_length")
-        q_estimator.summary_writer.add_summary(episode_summary, total_t)
+        q_estimator.summary_writer.add_summary(episode_summary, total_step)
         q_estimator.summary_writer.flush()
 
-        yield total_t, plotting.EpisodeStats(
-            episode_lengths=stats.episode_lengths[:i_episode+1],
-            episode_rewards=stats.episode_rewards[:i_episode+1])
 
     env.monitor.close()
     return stats
@@ -348,18 +359,18 @@ experiment_dir = os.path.abspath("./experiments/{}".format(env.spec.id))
 global_step = tf.Variable(0, name='global_step', trainable=False)
 
 # Create estimators
-q_estimator = Estimator(scope="q", summaries_dir=experiment_dir)
-target_estimator = Estimator(scope="target_q")
+main_model = DQN('main', summaries_dir=experiment_dir)
+target_model = DQN('target')
 
 # State processor
 state_processor = StateProcessor()
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for t, stats in deep_q_learning(sess,
+    for t in deep_q_learning(sess,
                                     env,
-                                    main_model=q_estimator,
-                                    target_model=target_estimator,
+                                    main_model=main_model,
+                                    target_model=target_model,
                                     state_processor=state_processor,
                                     experiment_dir=experiment_dir,
                                     num_episodes=10000,
@@ -371,5 +382,4 @@ with tf.Session() as sess:
                                     epsilon_decay_steps=500000,
                                     discount_factor=0.99,
                                     batch_size=32):
-
-        print("\nEpisode Reward: {}".format(stats.episode_rewards[-1]))
+        print("Step[", t, "]")
